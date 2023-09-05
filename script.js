363e230f7cf040333b37e507092085012802fd62
jjoonior <121348399+jjoonior@users.noreply.github.com>
2023-09-05 12:02:02 +0900

    fix: 광고 차단 해제 메시지 감지하여 삭제하도록 수정

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
-setTimeout(() => {
-  document.body.style.overflow = "visible";
-  const div = document.querySelector('.fc-ab-root');
-  if (div) {
-    div.remove();
+const targetNode = document.body;
+const config = { childList: true, subtree: true };
+
+const callback = (mutationsList, observer) => {
+  for (const mutation of mutationsList) {
+    if (mutation.type === 'childList') {
+      const addedNodes = mutation.addedNodes;
+      for (const addedNode of addedNodes) {
+        if (addedNode instanceof HTMLElement) {
+          if (document.querySelector('.fc-ab-root')) {
+            document.body.style.overflow = "visible";
+            addedNode.remove();
+          }
+        }
+      }
+    }
   }
-}, 3000);
+};
+
+const observer = new MutationObserver(callback);
+observer.observe(targetNode, config);
